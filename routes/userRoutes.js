9d115414989c1aa8745761928c5ee95da323c649
sarvendraSPO <sarvendra.spo@gmail.com>
2025-05-06 02:43:29 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./models/User.js');
+const User = require('models/User.js');
 
 // Create a new user
 router.post('/', async (req, res) => {
